e18cdd8c3df018894e139c3558cf7c779dfdcffd
Juanma L <juanmalubian@gmail.com>
2021-10-22 12:40:55 +0200

    Commit inicial

---
+++ README.md
@@ -0,0 +1,1 @@
+Reto config server. Bootcamp Everis
---
+++ src/main/java/com/nttdata/bootcamp/RetoConfigClientApplication.java
@@ -0,0 +1,13 @@
+package com.nttdata.bootcamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetoConfigClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetoConfigClientApplication.class, args);
+	}
+
+}
