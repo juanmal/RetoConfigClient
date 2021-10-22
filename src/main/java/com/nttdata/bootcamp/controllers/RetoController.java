2c244bcb5e2af4aa86211cb8d00966748327ce69
Juanma L <juanmalubian@gmail.com>
2021-10-22 14:19:53 +0200

    Errores solucionados

--- src/main/java/com/nttdata/bootcamp/controllers/RetoController.java
+++ src/main/java/com/nttdata/bootcamp/controllers/RetoController.java
@@ -11,9 +11,26 @@ public class RetoController {
 	
 	@Value("${some.value1}")
 	private String value1;
+
+	@Value("${some.value2}")
+	private String value2;
+
+	@Value("${some.value3}")
+	private String value3;
+
+	@Value("${some.value4}")
+	private String value4;
+
+	@Value("${some.value5}")
+	private String value5;
 	
-	@GetMapping("/value1")
-	public String value1() {
-		return this.value1;
+	@GetMapping("/values")
+	public String values() {
+		String cad = "Variable1: " + this.value1 
+				+ "\nVariable2: " + this.value2
+				+ "\nVariable3: " + this.value3
+				+ "\nVariable4: " + this.value4
+				+ "\nVariable5: " + this.value5;
+		return cad;
 	}
 }
